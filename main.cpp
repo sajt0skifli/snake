512ec192085f570d2752f0e5b8f4189aad6a2ee8
Sajtoskifli <77544877+Sajtoskifli001@users.noreply.github.com>
2022-03-13 21:36:20 +0100

    Add textures, update main
    Add fej.kep, test.kep, update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,49 @@
 #include "graphics.hpp"
 #include <vector>
+#include <fstream>
 
 using namespace genv;
 using namespace std;
 
-const int w=600,h=600,
+const int w=500,h=500,
           blocksize=20, //grid négyzet mérete (pixelben)
-          falvastag=2;  //pályaszéli fal vastagsága (blockban)
+          falvastag=2,  //pályaszéli fal vastagsága (blockban)
+          beltav=4;     //belsõ fal oldalfaltól való távolsága (blockban)
+
+struct pont
+{
+    int x,y;
+};
+
+struct szin
+{
+    int r,g,b;
+};
 
 struct block
 {
-    int x,y,status=0;
+    pont koord;
+    char status='s';    //s=szabad, f=fal, t=test, a=alma
 };
+
+struct fej
+{
+    private:
+        pont koord;
+        char irany='j'; //j=jobb, b=bal, l=le, f=fel
+};
+
+struct kigyo
+{
+    private:
+        pont koord;
+        kigyo* follow;
+};
+
+bool operator==(const pont &a, const pont &b)
+{
+    return (a.x==b.x and a.y==b.y);
+}
 
 void gridbuild(vector<vector<block>> &grid)
 {
@@ -21,19 +53,31 @@ void gridbuild(vector<vector<block>> &grid)
         {
             if(j==0)
             {
-                grid[i][j].x=0;
+                grid[i][j].koord.x=0;
             }
             else
             {
-                grid[i][j].x=grid[i][j-1].x+blocksize;
+                grid[i][j].koord.x=grid[i][j-1].koord.x+blocksize;
             }
             if(i==0)
             {
-                grid[i][j].y=0;
+                grid[i][j].koord.y=0;
             }
             else
             {
-                grid[i][j].y=grid[i-1][j].y+blocksize;
+                grid[i][j].koord.y=grid[i-1][j].koord.y+blocksize;
+            }
+        }
+    }
+
+    for(int i=0;i<grid.size();i++)
+    {
+        for(int j=0;j<grid[i].size();j++)
+        {
+            if(i<falvastag || j<falvastag || i>grid.size()-falvastag-1 || j>grid[i].size()-falvastag-1 ||  //oldalfalak
+               (i==falvastag+beltav && j>=falvastag+beltav && j<=grid[i].size()-(falvastag+beltav)-1) || (j==grid[i].size()/2 && i>=falvastag+beltav && i<=grid.size()-(falvastag+beltav)-1))    //belsõ fal
+            {
+                grid[i][j].status='f';
             }
         }
     }
@@ -45,16 +89,40 @@ void hatterbuild(canvas &hatter, vector<vector<block>> grid)
     {
         for(int j=0;j<grid[i].size();j++)
         {
-            if(i<falvastag || j<falvastag || i>grid.size()-falvastag-1 || j>grid[i].size()-falvastag-1)
+            if(grid[i][j].status=='f')
             {
-                grid[i][j].status=1;
                 hatter << color(100,100,100);
             }
             else
             {
                 hatter << color(0,200,0);
             }
-            hatter << move_to(grid[i][j].x,grid[i][j].y) << box(blocksize,blocksize);
+            hatter << move_to(grid[i][j].koord.x,grid[i][j].koord.y) << box(blocksize,blocksize);
+        }
+    }
+}
+
+void pattern(canvas &c ,vector<vector<szin>> kep)
+{
+    for(int i=0;i<kep.size();i++)
+    {
+        for(int j=0;j<kep[i].size();j++)
+        {
+            c << move_to(j,i) << color(kep[i][j].r,kep[i][j].g,kep[i][j].b) << dot;
+        }
+    }
+}
+
+void import(ifstream &be, vector<vector<szin>> &kep)
+{
+    int szel, mag;
+    be >> szel >> mag;
+    kep=vector<vector<szin>>(mag,vector<szin>(szel));
+    for(int i=0;i<kep.size();i++)
+    {
+        for(int j=0;j<kep[i].size();j++)
+        {
+            be >> kep[i][j].r >> kep[i][j].g >> kep[i][j].b;
         }
     }
 }
@@ -62,12 +130,31 @@ void hatterbuild(canvas &hatter, vector<vector<block>> grid)
 int main()
 {
     vector<vector<block>> grid(h/blocksize,vector<block>(w/blocksize));
-    canvas hatter;
-    gout.open(w,h);
-    hatter.open(w,h);
-    gridbuild(grid);
-    hatterbuild(hatter,grid);
+    vector<vector<szin>> kep;
+    ifstream be;
+
+    canvas hatter,fej,test;                 //
+    gout.open(w,h);                         //
+    hatter.open(w,h);                       //  Grid és háttér létrehozása
+    gridbuild(grid);                        //
+    hatterbuild(hatter,grid);               //
+
+    be.open("fej.kep");                     //
+    import(be,kep);                         //
+    fej.open(kep[0].size(),kep.size());     //  Fej textúra importálása
+    pattern(fej,kep);                       //
+    be.close();                             //
+
+    be.open("test.kep");                    //
+    import(be,kep);                         //
+    test.open(kep[0].size(),kep.size());    //  Test textúra importálása
+    pattern(test,kep);                      //
+    be.close();                             //
+
     gout << stamp(hatter,0,0) << refresh;
+    gout << stamp(fej,0,0) << refresh;
+    gout << stamp(test,20,0) << refresh;
+
     event ev;
     while(gin >> ev) {
         if(ev.type==ev_key && ev.keycode == key_escape)
