62758f97e8cb9be5495028e4511521a77dba11f0
Sajtoskifli <77544877+Sajtoskifli001@users.noreply.github.com>
2022-03-19 16:02:20 +0100

    Update main
    Playable build, includes menu and gameover

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ using namespace genv;
 using namespace std;
 
 const int w=700,h=700,
-          blocksize=20, //rács négyzet mérete (pixelben) - a program 20 köré épült, ennek megváltoztatásával nem mûködik
+          blocksize=20, //rács négyzet mérete (pixelben) - a program 20 köré épült, ennek megváltoztatása világkatasztrófát eredményez (és még a program sem fog mûködni)
           falvastag=2,  //pályaszéli oldalfal vastagsága (blockban)
-          beltav=4,     //belso fal oldalfaltól való távolsága (blockban)
-          almatime=8;   //alma létrehozásának gyakorisága (lépésben) (pl.: megevéstõl számított 7. lépésben)
+          beltav=4;     //belso fal oldalfaltól való távolsága (blockban)
 
 struct pont
 {
@@ -33,6 +32,139 @@ bool operator==(const pont &a, const pont &b)
     return (a.x==b.x and a.y==b.y);
 }
 
+struct Textpanel
+{
+    protected:
+        pont koord;
+        szin txtcolor;
+        int szel,mag;
+        string szoveg;
+
+    public:
+    Textpanel(string s)
+    {
+        szel=w/3;
+        mag=h/10;
+        koord.x=w/2-szel/2;
+        koord.y=h-(mag*3.5);
+        szoveg=s;
+        txtcolor.r=255;
+        txtcolor.g=120;
+        txtcolor.b=0;
+    }
+
+    virtual void rajzol()
+    {
+        gout.load_font("LiberationMono-Bold.ttf",w/20);
+        gout << color(67,67,67) << move_to(koord.x,koord.y) << box(szel,mag)
+             << color(100,100,100) << move_to(koord.x+5,koord.y+5) << box(szel-10,mag-10)
+
+             << color(txtcolor.r,txtcolor.g,txtcolor.b)
+             << move_to(koord.x+(szel/2)-(gout.twidth(szoveg)/2),koord.y+(mag/2)-((gout.cascent()+gout.cdescent())/2))
+             << text(szoveg);
+    }
+
+    bool rajta(int cx, int cy)
+    {
+        return (cx>koord.x and cx<koord.x+szel and cy>koord.y and cy<koord.y+mag);
+    }
+
+    void colorchange(szin becolor)
+    {
+        txtcolor=becolor;
+    }
+};
+
+struct Title:public Textpanel
+{
+    protected:
+        string alcim;
+
+    public:
+    Title(string s, string als): Textpanel(s)
+    {
+        koord.x=w/7;
+        koord.y=h/7;
+        szel=w/7*5;
+        mag=h/5;
+        alcim=als;
+        txtcolor.r=0;
+        txtcolor.g=255;
+        txtcolor.b=0;
+    }
+
+    void rajzol() override
+    {
+        gout.load_font("LiberationMono-Bold.ttf",w/14);
+        gout << color(100,0,0) << move_to(koord.x,koord.y) << box(szel,mag)
+             << color(160,0,0) << move_to(koord.x+10,koord.y+10) << box(szel-20,mag-20)
+
+             << color(txtcolor.r,txtcolor.g,txtcolor.b) << move_to(w/2-(gout.twidth(szoveg)/2),koord.y+(mag/2)-5-(gout.cascent()+gout.cdescent())/2)
+             << text(szoveg);
+        int txtmag=gout.cascent()+gout.cdescent();
+        gout.load_font("LiberationMono-Bold.ttf",w/50);
+        gout << move_to(w/2-(gout.twidth(alcim)/2),koord.y+(mag/2)+(txtmag/2)-(gout.cascent()+gout.cdescent())/2)
+             << text(alcim);
+    }
+};
+
+struct Gomb:public Textpanel
+{
+    protected:
+        bool jelolt=false;
+
+    public:
+    Gomb(int x_, int y_, string s, int r_, int g_, int b_): Textpanel(s)
+    {
+        szel=w/5;
+        mag=h/10;
+        koord.x=x_-szel/2;
+        koord.y=y_-mag/2;
+        txtcolor.r=r_;
+        txtcolor.g=g_;
+        txtcolor.b=b_;
+    }
+
+    void rajzol() override
+    {
+        gout.load_font("LiberationMono-Bold.ttf",w/28);
+        if(jelolt)
+        {
+            gout << color(txtcolor.r,txtcolor.g,txtcolor.b);
+        }
+        else
+        {
+            gout << color(67,67,67);
+        }
+        gout << move_to(koord.x,koord.y) << box(szel,mag)
+             << color(100,100,100) << move_to(koord.x+5,koord.y+5) << box(szel-10,mag-10)
+
+             << color(txtcolor.r,txtcolor.g,txtcolor.b)
+             << move_to(koord.x+(szel/2)-(gout.twidth(szoveg)/2),koord.y+(mag/2)-((gout.cascent()+gout.cdescent())/2))
+             << text(szoveg);
+    }
+
+    void jelolvalt(bool b)
+    {
+        jelolt=b;
+    }
+
+    int difficulty()
+    {
+        if(szoveg=="Hard")
+            return 200;
+        else if(szoveg=="Easy")
+            return 400;
+        else
+            return 300;
+    }
+
+    szin colorker()
+    {
+        return txtcolor;
+    }
+};
+
 struct Racs
 {
     private:
@@ -42,9 +174,9 @@ struct Racs
     Racs()
     {
         grid=vector<vector<block>>(h/blocksize,vector<block>(w/blocksize));
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(j==0)
                 {
@@ -62,14 +194,10 @@ struct Racs
                 {
                     grid[i][j].koord.y=grid[i-1][j].koord.y+blocksize;
                 }
-            }
-        }
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[i].size();j++)
-            {
+
                 if(i<falvastag || j<falvastag || i>grid.size()-falvastag-1 || j>grid[i].size()-falvastag-1 ||  //oldalfalak
-                   (i==falvastag+beltav && j>=falvastag+beltav && j<=grid[i].size()-(falvastag+beltav)-1) || (j==grid[i].size()/2 && i>=falvastag+beltav && i<=grid.size()-(falvastag+beltav)-1))    //belsÃµ fal
+                   (i==falvastag+beltav && j>=falvastag+beltav && j<=grid[i].size()-(falvastag+beltav)-1) ||   // belsõ
+                   (j==grid[i].size()/2 && i>=falvastag+beltav && i<=grid.size()-(falvastag+beltav)-1))        //  fal
                 {
                     grid[i][j].status='f';
                 }
@@ -79,9 +207,9 @@ struct Racs
 
     void hatter(canvas &hatter, canvas fu, canvas fal)
     {
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j].status=='f')
                 {
@@ -107,9 +235,9 @@ struct Racs
 
     void testclear()
     {
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j].status=='t')
                 {
@@ -125,9 +253,9 @@ struct Racs
 
     void testadd(pont testpoz)
     {
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j].koord==testpoz)
                 {
@@ -141,9 +269,9 @@ struct Racs
     {
         block* nextblock;
         bool volt=false;
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j].koord==fejpoz && !volt)
                 {
@@ -177,9 +305,9 @@ struct Racs
 
     void ghostmode(canvas c)
     {
-        for(int i=0;i<grid.size();i++)
+        for(size_t i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(size_t j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j].status=='p')
                 {
@@ -333,17 +461,17 @@ struct Alma
     }
 };
 
-void import(ifstream &be, string fajl, canvas &c)
+void import(string fajl, canvas &c)
 {
+    ifstream be(fajl);
     vector<vector<szin>> kep;
     int szel, mag;
-    be.open(fajl);
     be >> szel >> mag;
     c.open(szel,mag);
     kep=vector<vector<szin>>(mag,vector<szin>(szel));
-    for(int i=0;i<kep.size();i++)
+    for(size_t i=0;i<kep.size();i++)
     {
-        for(int j=0;j<kep[i].size();j++)
+        for(size_t j=0;j<kep[i].size();j++)
         {
             be >> kep[i][j].r >> kep[i][j].g >> kep[i][j].b;
             c << move_to(j,i) << color(kep[i][j].r,kep[i][j].g,kep[i][j].b) << dot;
@@ -352,63 +480,99 @@ void import(ifstream &be, string fajl, canvas &c)
     be.close();
 }
 
+void gombrajzol(Title cim, vector<Gomb*> gombok, Textpanel start)
+{
+    cim.rajzol();
+    for(size_t i=0;i<gombok.size();i++)
+    {
+        gombok[i]->rajzol();
+    }
+    start.rajzol();
+    gout << refresh;
+}
+
 void rajzol(canvas hatter, Fej fej, vector<Test*> testek, Alma alma, int pontszam)
 {
+    gout.load_font("LiberationMono-Bold.ttf",20);
     stringstream ss;
     gout << stamp(hatter,0,0);
-    for(int i=0;i<testek.size();i++)
+    for(size_t i=0;i<testek.size();i++)
     {
         testek[i]->rajzol();
     }
     alma.rajzol();
     fej.rajzol();
     ss << "Pontok: " << pontszam;
-    gout << color(0,255,0) << move_to((w/2)-gout.twidth(ss.str())/2,blocksize/2) << font("LiberationMono-Bold.ttf",20) << text(ss.str());
+    gout << color(0,255,0) << move_to((w/2)-gout.twidth(ss.str())/2,blocksize/2) << text(ss.str())
+         << refresh;
 }
 
 int main()
 {
-    ifstream be;
-    canvas hatter,fejle,fejfel,fejjobb,fejbal,fejdead,test,fal,fu,almatex;
-    Racs racs;
-    bool gamestart=false, gameover=false;
+    bool gamestart=true, gameover=false;
     char inputirany='j', fejstatus;
-    int almakor=0,pontszam=0;
+    int almakor=0, pontszam=0;
+    const int almatime=8;  //almatime: alma létrehozásának gyakorisága (lépésben) (pl.: megevéstõl számított 8. lépésben)
     gout.open(w,h);
 
-    import(be,"fej2le.kep",fejle);          //
-    import(be,"fej2fel.kep",fejfel);        //
-    import(be,"fej2jobb.kep",fejjobb);      //
-    import(be,"fej2bal.kep",fejbal);        //   Textúrák
-    import(be,"fej2dead.kep",fejdead);      //  importálása
-    import(be,"test.kep",test);             //
-    import(be,"fal.kep",fal);               //
-    import(be,"fu.kep",fu);                 //
-    import(be,"alma.kep",almatex);          //
+    canvas hatter,fejle,fejfel,fejjobb,fejbal,fejdead,test,fal,fu,almatex;
+    import("fej2le.kep",fejle);          //
+    import("fej2fel.kep",fejfel);        //
+    import("fej2jobb.kep",fejjobb);      //
+    import("fej2bal.kep",fejbal);        //   Textúrák
+    import("fej2dead.kep",fejdead);      //  importálása
+    import("test.kep",test);             //
+    import("fal.kep",fal);               //
+    import("fu.kep",fu);                 //
+    import("alma.kep",almatex);          //
 
-    hatter.open(w,h);                                                                                       //
-    racs.hatter(hatter,fu,fal);                                                                             //   Rács és háttér
-    gout << stamp(hatter,0,0) << font("LiberationMono-Bold.ttf",20);                                        //     létrehozása
-    gout << color(0,255,0) << move_to((w/2)-gout.twidth("Pontok: 0")/2,blocksize/2) << text("Pontok: 0");   //
-
+    Alma alma(almatex);                  //alma létrehozása
+    Racs racs;                           //
+    hatter.open(w,h);                    //  Rács és háttér létrehozása
+    racs.hatter(hatter,fu,fal);          //
     vector<Test*> testek;                                                                                  //
-    Fej fej(racs.xblockszam(),racs.yblockszam(),fejjobb);                                                  //
-    fej.rajzol();                                                                                          //
-    testek.push_back(new Test(fej.pozicio().x-blocksize,fej.pozicio().y,0,test));                          //  Fej és test
-    testek[0]->rajzol();                                                                                   //  alapállapota
+    Fej fej(racs.xblockszam(),racs.yblockszam(),fejjobb);                                                  //                                                                                          //
+    testek.push_back(new Test(fej.pozicio().x-blocksize,fej.pozicio().y,0,test));                          //  Fej és test                                                                             //  alapállapota
     testek.push_back(new Test(testek[0]->pozicio().x-blocksize,testek[0]->pozicio().y,testek[0],test));    //
-    testek[1]-> rajzol();                                                                                  //
-    gout << refresh;                                                                                       //
+    rajzol(hatter,fej,testek,alma,pontszam);                                                               //                                                                                     //
 
-    Alma alma(almatex);                     //Alma létrehozása
+    vector<Gomb*> gombok;
+    gombok.push_back(new Gomb(w/4,h/2,"Easy",0,255,0));
+    gombok.push_back(new Gomb(w/2,h/2,"Normal",255,120,0));
+    gombok.push_back(new Gomb(w/4*3,h/2,"Hard",255,0,0));
+    Gomb* jelolt=gombok[1];
+    jelolt->jelolvalt(true);
+    Title cim("Epic Snake Game","or something");
+    Textpanel start("Start");
+    gombrajzol(cim,gombok,start);
 
-    gin.timer(400);
     event ev;
-    while(gin >> ev) {
-        if(ev.type==ev_key && ev.keycode==key_escape)
+    while(gin >> ev && ev.keycode!=key_escape) {
+        if(gamestart)
         {
-            return 0;
+            if(ev.type==ev_mouse && ev.button==btn_left)
+            {
+                for(size_t i=0;i<gombok.size();i++)
+                {
+                    gombok[i]->jelolvalt(false);
+                    if(gombok[i]->rajta(ev.pos_x,ev.pos_y))
+                    {
+                        jelolt=gombok[i];
+                    }
+                }
+                jelolt->jelolvalt(true);
+                start.colorchange(jelolt->colorker());
+                gombrajzol(cim,gombok,start);
+
+                if(start.rajta(ev.pos_x,ev.pos_y))
+                {
+                    gout.showmouse(false);
+                    gamestart=false;
+                }
+            }
+            gin.timer(jelolt->difficulty());
         }
+
         else if(!gamestart && !gameover)
         {
             if(ev.type==ev_timer)
@@ -448,8 +612,9 @@ int main()
                 if(gameover)
                 {
                     racs.ghostmode(test);
+                    gout.showmouse(true);
+                    gout << refresh;
                 }
-                gout << refresh;
             }
 
             else if(ev.type==ev_key)
@@ -469,11 +634,25 @@ int main()
                     inputirany='l';
                     fej.texreplace(fejle);
                 }
-                if(ev.keycode==key_left && fej.iranyker()!='j')
+                else if(ev.keycode==key_left && fej.iranyker()!='j')
                 {
                     inputirany='b';
                     fej.texreplace(fejbal);
                 }
+            }
+        }
+
+        else
+        {
+            Title ending("Snake perished!","and it's because of you...");
+            Textpanel exit("Exit");
+            exit.rajzol();
+            ending.rajzol();
+            gout << refresh;
+
+            if(ev.type==ev_mouse && ev.button==btn_left && exit.rajta(ev.pos_x,ev.pos_y))
+            {
+                return 0;
             }
         }
     }
